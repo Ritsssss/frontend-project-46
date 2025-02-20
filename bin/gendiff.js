7d4a4c73f96133f38a1356e555fda5ce8802c278
rita sh <lapandh@gmail.com>
2025-02-20 14:08:58 -0500

    Edit gendiff to take 2 arguments; Add option -f, format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,8 +6,8 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0', '-V, --version', 'output the version number');
-
-program.addHelpText('before', 'gendiff -h\n');
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
