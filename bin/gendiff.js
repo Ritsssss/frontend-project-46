6de84a87ea4e18110ed5a7aa24d5ccc201f04b96
rita sh <lapandh@gmail.com>
2025-02-19 21:59:27 -0500

    Clone repository; create gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number');
+
+program.addHelpText('before', 'gendiff -h\n');
+
+program.parse();
